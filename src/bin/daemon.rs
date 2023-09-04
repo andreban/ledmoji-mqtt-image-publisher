a84c207fb1a8fbfaf27c626ca6ee9d10e7302911
André Cipriani Bandarra <andreban@gmail.com>
2023-09-04 20:15:08 +0100

    Adds a timeout when waiting for chunks

--- src/bin/daemon.rs
+++ src/bin/daemon.rs
@@ -69,7 +69,13 @@ async fn main() -> Result<(), Box<dyn Error>> {
         };
 
         loop {
-            let Ok(Some(chunk)) = response.chunk().await else {
+            let Ok(chunk) = tokio::time::timeout(Duration::from_secs(60), response.chunk()).await
+            else {
+                log::error!("Timed out getting chunk");
+                break;
+            };
+
+            let Ok(Some(chunk)) = chunk else {
                 log::error!("Failed to get chunk");
                 break;
             };
